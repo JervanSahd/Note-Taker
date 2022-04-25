d3728efc5a07d27d2650e330f54fc287be92ff4d
S.Snavely <info@stevesnavely.com>
2022-04-25 08:31:33 -0400

    Update Code

--- routes/notesRouter.js
+++ routes/notesRouter.js
@@ -34,12 +34,12 @@ res.json('Note added successfully');
 );
 
 // GET Route for homepage
-app.get('/', (req, res) =>
+notes.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 // Wildcard route to direct users to the home page
-app.get('*', (req, res) =>
+notes.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/pages/index.html'))
 );
 
